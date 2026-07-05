1303a0e53684a0a13684a5a49ce4856ddfa8e9fe
ouyangliteng <67260777@qq.com>
2026-07-05 11:01:32 +0800

    Fix upload auth session reset

--- src/app.tsx
+++ src/app.tsx
@@ -1,32 +1,15 @@
-import { useEffect, useRef } from 'react'
-import Taro, { useDidHide, useDidShow } from '@tarojs/taro'
+import { useEffect } from 'react'
+import Taro from '@tarojs/taro'
 import { useAuthStore } from './store/authStore'
 import './app.scss'
 
 function App({ children }: { children: React.ReactNode }) {
-  const firstShowRef = useRef(true)
   const { logout } = useAuthStore()
 
   useEffect(() => {
     clearSession()
     logout()
   }, [logout])
-
-  useDidHide(() => {
-    clearSession()
-    logout()
-  })
-
-  useDidShow(() => {
-    if (firstShowRef.current) {
-      firstShowRef.current = false
-      return
-    }
-    const token = Taro.getStorageSync('token')
-    if (!token) {
-      Taro.reLaunch({ url: '/pages/login/index' }).catch(() => {})
-    }
-  })
 
   return <>{children}</>
 }
